9583102b6868fee0337014acae8a70d97aadc856
Alex Robertson <alex@someoddpilot.com>
2015-01-12 09:36:33 -0600

    Adding limiting
    Passing an options object in the glob object with a count parameter will
    limit collected items.

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ var glob = require("glob");
 var _ = require("lodash");
 
 module.exports = function (fileGlobs) {
+  var options = fileGlobs.options;
+
+  delete fileGlobs.options;
+
   function collectionsTransform(file, enc, callback) {
     var collections = {};
 
     Object.keys(fileGlobs).forEach(function (name) {
       var files = glob.sync(fileGlobs[name]);
-      collections[name] = files.map(function (filepath) {
+
+      var collected = files.map(function (filepath) {
         var contents = fs.readFileSync(filepath, "utf-8");
 
         return _.extend(
@@ -21,6 +26,12 @@ module.exports = function (fileGlobs) {
           frontMatter(contents)
         );
       });
+
+      if (options.count) {
+        collected = collected.slice(0, options.count);
+      }
+
+      collections[name] = collected;
     });
 
     file.collections = collections;
