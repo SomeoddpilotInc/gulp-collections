821c7f2384f96d41d415f30d4b42998194003c8d
Alex Robertson <alex@someoddpilot.com>
2015-05-21 14:43:52 -0500

    Major refactoring and flattening of code

--- src/index.js
+++ src/index.js
@@ -5,52 +5,52 @@ var through = require("through2");
 var glob = require("glob");
 var _ = require("lodash");
 
+function mapFiles(filepath) {
+  var contents = fs.readFileSync(filepath, "utf-8");
+
+  return _.extend(
+    {
+      slug: path.basename(filepath, path.extname(filepath))
+    },
+    frontMatter(contents)
+  );
+}
+
+function forEachFileGlob(collections, fileGlobs, options, name) {
+  var collection = fileGlobs[name];
+
+  var files = glob.sync(collection.glob || collection);
+
+  collections[name] = files
+    .map(mapFiles)
+    .sort(function (a, b) {
+      var sortBy = collection.sortBy || options.sortBy || null;
+
+      if (typeof sortBy !== 'function') {
+        return -1;
+      }
+      return sortBy(a, b);
+    })
+    .slice(
+      0,
+      collection.count || options.count || files.length
+    );
+}
+
 module.exports = function (options) {
   var fileGlobs = options.globs || {};
-
-  options = options || {};
 
   function collectionsTransform(file, enc, callback) {
     var collections = {};
 
-    Object.keys(fileGlobs).forEach(function (name) {
-      var collection = fileGlobs[name];
-
-      var fileGlob = (collection.glob) ? collection.glob : collection;
-
-      var files = glob.sync(fileGlob);
-
-      var collected = files.map(function (filepath) {
-        var contents = fs.readFileSync(filepath, "utf-8");
-
-        return _.extend(
-          {
-            slug: path.basename(filepath, path.extname(filepath))
-          },
-          frontMatter(contents)
-        );
-      });
-
-      var sortBy = collection.sortBy || options.sortBy || null;
-
-      if (sortBy) {
-        collected = collected.sort(sortBy);
-      }
-
-      var count = collection.count || options.count || null;
-
-      if (count) {
-        collected = collected.slice(0, count);
-      }
-
-      collections[name] = collected;
-    });
+    Object.keys(fileGlobs)
+      .forEach(forEachFileGlob.bind(this, collections, fileGlobs, options || {}));
 
     file.collections = collections;
 
     this.push(file);
 
-    callback();
+    callback(null, file);
   }
 
   return through.obj(collectionsTransform);
